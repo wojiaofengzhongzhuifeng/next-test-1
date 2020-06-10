dbc818717fd45e341e2a0901335d821a7ce5328e
饶家俊 <raojiajun@bimwinner.com>
2020-06-10 09:41:21 +0800

    使用库渲染 md 文件

--- pages/api/v1/posts.ts
+++ pages/api/v1/posts.ts
@@ -1,7 +1,20 @@
 import {NextApiRequest, NextApiResponse} from "next";
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 
+const rootDirPath = process.cwd();
+
+const firstMDFilePath = path.join(rootDirPath, 'markdown/01.md');
+
+fs.readFile(firstMDFilePath, (error, data)=>{
+  if(error)console.error(error);
+  const mdContent = data.toString();
+  console.log(matter(mdContent));
+})
 const test = (request: NextApiRequest, response: NextApiResponse) => {
-  response.end(JSON.stringify({a: 1}))
+  // response.setHeader('Content-Type', 'application/json')
+  response.end(JSON.stringify({a: 2}))
 }
 
 export default test
